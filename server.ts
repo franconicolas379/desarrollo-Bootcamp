93dfba571f0dcf28d3dda9b262b5984cf3cd89a7
Medrano Franco Nicolas <franconicolas379@gmail.com>
2024-09-06 19:02:17 -0300

    Correccion y ordenacion de server.ts

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-import express, { application, Request, Response } from "express";
+import express, { Request, Response } from "express";
 import {usersRouter, adminRouter, authRouter} from "./routes";
 import dbConnect from "./db/dbConnect";
 import { config } from "dotenv";
@@ -6,24 +6,19 @@ import { config } from "dotenv";
 config();
 
 const PORT = Number(process.env.PORT) ?? 3000;
-const HOST = process.env.HOST ?? "localhost";
+const HOST = process.env.HOST!;
 
-application.use("/user", usersRouter);
-application.use("/admin", adminRouter);
-application.use("/auth", authRouter);
+const app = express();
 
-application.use(express.json());
+app.use(express.json());
 
-application.listen(PORT, HOST, () => {
-    console.log(`server is running on http://${HOST}:${PORT}`);
-})
-
-application.get("/", (req: Request, res: Response) => {
-    res.send("Pagina de Inicio");
-})
+app.use("/user", usersRouter);
+app.use("/admin", adminRouter);
+app.use("/auth", authRouter);
 
 dbConnect();
 
-application.get("/Admin", (req: Request, res: Response) => {
-    res.send("Admin Page");
+app.listen(PORT, HOST, () => {
+    console.log(`server is running on http://${HOST}:${PORT}`);
 })
+
