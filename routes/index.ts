b9aca9c0452c93a9ef7c06835ca8ad33254b68a6
Medrano Franco Nicolas <franconicolas379@gmail.com>
2024-09-08 16:33:01 -0300

    se actualiza el index para utilizar solo las rutas de la api y por ende se eliminan las demas

--- routes/index.ts
+++ routes/index.ts
@@ -1,3 +1,10 @@
-export {default as usersRouter} from "./user";
-export {default as adminRouter} from "./admin";
-export {default as authRouter} from "./auth";
+import express from "express";
+import userRouter from "../api/user/routes";
+import productRouter from "../api/product/routes";
+
+const router = express.Router();
+
+router.use("/users", userRouter);
+router.use("/products", productRouter);
+
+export default router;
